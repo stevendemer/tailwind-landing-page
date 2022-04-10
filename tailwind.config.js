39dcbf0282fc25cd106057be4b2e7b9926698985
Steven Demertzis <devil.jaws@gmail.com>
2022-04-10 15:51:02 +0300

    Changed scripts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./*.html'],
   theme: {
     screens: {
       sm: '480px',
       md: '768px',
       lg: '976px',
-      xl: '1440px'
+      xl: '1440px',
     },
     extend: {
       colors: {
@@ -13,11 +13,11 @@ module.exports = {
         brightRedLight: 'hsl(12, 88%, 69%)',
         brightRedSupLight: 'hsl(12, 88%, 95%)',
         darkBlue: 'hsl(228, 39%, 23%)',
-        darkGrayBlue: 'hsl(227, 12%, 61%)',
-        veryDarkBlue: 'hsl(223, 12%, 13%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
         veryPaleRed: 'hsl(13, 100%, 96%)',
-        veryLightGray: 'hsl(0, 0%, 98%)'
-      }
+        veryLightGray: 'hsl(0, 0%, 98%)',
+      },
     },
   },
   plugins: [],
